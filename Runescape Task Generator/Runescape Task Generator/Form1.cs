b5ea8531b281a2f9571bdad35427e8940884920f
Matbart1 <mathieubarrette9@gmail.com>
2023-10-27 23:54:08 -0600

    Initial commit

---
+++ Runescape Task Generator/Runescape Task Generator/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Runescape_Task_Generator
+{
+    public partial class RSGenerator : Form
+    {
+        public RSGenerator()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class MonsterInfo
+    {
+
+    }
+
+    public class MonsterInfoArray
+    {
+        public MonsterInfo[] fullInfo { get; set; }
+    }
+}
